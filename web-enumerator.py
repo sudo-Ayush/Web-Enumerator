a614e859443f6490242af3842fa7a57c99d10701
Ayush Chourasia <76032725+sudo-Ayush@users.noreply.github.com>
2022-11-26 17:57:29 +0530

    Update web-enumerator.py

--- web-enumerator.py
+++ web-enumerator.py
@@ -5,6 +5,7 @@ from logic import *
 import threading
 import colorama
 import requests
+import whois
 import time
 
 colorama.init(autoreset=True)
@@ -59,36 +60,14 @@ def host_diagnostics(domain):
         _ = system(f'host {domain}')
     return
 
-def whois_info(domain):
-    print(f"{Fore.YELLOW}[*] Whois info...\n")
-    whois = f"https://who.is/whois/{domain}"
-    response = requests.get(whois)
-    soup = BeautifulSoup(response.text , 'html.parser')
-    r_info = soup.findAll("div",{"class": "col-md-12 queryResponseBodyValue"})[1]
-    print(f'{r_info.text}\n')
-
-    print(f"{Fore.YELLOW}[*] Enumerating Name Servers...\n")
-
-    NS = f"https://who.is/dns/{domain}"
-
-    NS_data = []
-    NS_response = requests.get(NS)
-    soup = BeautifulSoup(NS_response.text , 'html.parser')
-    NS_info = soup.find("table",{"class": "table"})
-    table_body = NS_info.find('tbody')
-
-    rows = table_body.find_all('tr')
-    
-    for row in rows:
-        cols = row.find_all('td')
-        cols = [ele.text.strip() for ele in cols]
-        NS_data.append([ele for ele in cols if ele])
-        
-    print('\n'.join(map(str, NS_data)))
+def whois_enum(domain):
+    print(f"{Fore.YELLOW}\n[*] Whois Info...\n")
+    time.sleep(2)
+    print(f"{Fore.GREEN}{whois.whois(domain)}")
     
     print(f"{Fore.YELLOW}\n[*] Enumerating Sub-domain...\n")
-    print(f"{Fore.LIGHTCYAN_EX}[+] All Discovered Sub-domains\n")
-    print(f"{Fore.LIGHTMAGENTA_EX}  [STATUS]  |      [SUB-DOMAIN]")
+    print(f"{Fore.LIGHTGREEN_EX}[+] All Discovered Sub-domains\n")
+    print(f"{Fore.LIGHTCYAN_EX}  [STATUS]  |      [SUB-DOMAIN]")
     print(f"{Fore.LIGHTWHITE_EX}-"*36)
     
 def sub_enum(sub, domain):
@@ -99,7 +78,7 @@ def sub_enum(sub, domain):
             print(f"{Fore.LIGHTGREEN_EX}->  {sub_try.status_code}     |  ",f"{Fore.GREEN}{sub_url}")
             
         elif sub_try.status_code >= 300 and sub_try.status_code < 400:
-            print(f"{Fore.LIGHTCYAN_EX}->  {sub_try.status_code}     |  ",f"{Fore.CYAN}{sub_url}")
+            print(f"{Fore.LIGHTMAGENTA_EX}->  {sub_try.status_code}     |  ",f"{Fore.MAGENTA}{sub_url}")
             
         elif sub_try.status_code >= 400 and sub_try.status_code < 599:
             print(f"{Fore.LIGHTRED_EX}->  {sub_try.status_code}     |  ",f"{Fore.RED}{sub_url}")
@@ -117,7 +96,7 @@ def main():
     start = time.time()
     initial_info(domain)
     host_diagnostics(domain)
-    whois_info(domain)
+    whois_enum(domain)
     
     with open('domains.txt', 'r') as domains:
         domains = domains.readlines()
